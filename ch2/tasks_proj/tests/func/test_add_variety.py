bd2c29fa3287df18f81c0ab5fb7362082a3b421e
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-18 17:40:52 +0900

    Page53 2-16
    (TestDrivePython) (anaconda2-4.1.0) hiratamasayuki-no-MacBook-Air:tasks_proj MasayukiHirata$ pytest -v tests/func/test_add_variety.py::test_add_4
    ============================================================ test session starts =============================================================
    platform darwin -- Python 3.7.2, pytest-4.2.0, py-1.7.0, pluggy-0.8.1 -- /Users/MasayukiHirata/anaconda3/envs/TestDrivePython/bin/python
    cachedir: .pytest_cache
    rootdir: /Users/MasayukiHirata/my-project/TestDrivePython/ch2/tasks_proj/tests, inifile: pytest.ini
    collected 5 items
    
    tests/func/test_add_variety.py::test_add_4[task0] PASSED                                                                               [ 20%]
    tests/func/test_add_variety.py::test_add_4[task1] PASSED                                                                               [ 40%]
    tests/func/test_add_variety.py::test_add_4[task2] PASSED                                                                               [ 60%]
    tests/func/test_add_variety.py::test_add_4[task3] PASSED                                                                               [ 80%]
    tests/func/test_add_variety.py::test_add_4[task4] PASSED                                                                               [100%]
    
    ========================================================== 5 passed in 0.27 seconds ==========================================================

--- ch2/tasks_proj/tests/func/test_add_variety.py
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -34,6 +34,19 @@ def test_add_3(summary, owner, done):
     assert equivalent(t_from_db, task)
 
 
+tasks_to_try = (Task('sleep', done=True),
+                Task('wake', 'brian'),
+                Task('wake', 'brian'),
+                Task('breathe', 'BRIAN', True),
+                Task('exercise', 'BrIaN', False))
+@pytest.mark.parametrize('task', tasks_to_try)
+def test_add_4(task):
+    """Slightly different take."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
 def equivalent(t1, t2):
     """Check two tasks for equivalance."""
     return ((t1.summary == t2.summary) and (t1.owner == t2.owner) and (t1.done == t2.done))
