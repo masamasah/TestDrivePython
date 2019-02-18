d88dda2a278836e2e2a81c0f4785b90752cf8b68
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-18 17:19:03 +0900

    Page47 2-12
    (TestDrivePython) (anaconda2-4.1.0) hiratamasayuki-no-MacBook-Air:tasks_proj MasayukiHirata$ pytest -v tests/func/test_api_exceptions.py::TestUpdate
    ============================================================ test session starts =============================================================
    platform darwin -- Python 3.7.2, pytest-4.2.0, py-1.7.0, pluggy-0.8.1 -- /Users/MasayukiHirata/anaconda3/envs/TestDrivePython/bin/python
    cachedir: .pytest_cache
    rootdir: /Users/MasayukiHirata/my-project/TestDrivePython/ch2/tasks_proj/tests, inifile: pytest.ini
    collected 2 items
    
    tests/func/test_api_exceptions.py::TestUpdate::test_bad_id PASSED                                                                      [ 50%]
    tests/func/test_api_exceptions.py::TestUpdate::test_bad_task PASSED                                                                    [100%]
    
    ========================================================== 2 passed in 0.10 seconds ==========================================================

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -1,5 +1,6 @@
 import pytest
 import tasks
+from tasks import Task
 
 def test_add_raises():
     """add() should raise an exception with wrong type param."""
@@ -25,3 +26,15 @@ def test_get_raises():
     """get() should raise an exception with wrong type param."""
     with pytest.raises(TypeError):
         tasks.get(task_id='123')
+
+class TestUpdate():
+    """Test expected exceptions with tasks.update()."""
+    def test_bad_id(self):
+        """A non-int id should raise an exception."""
+        with pytest.raises(TypeError):
+            tasks.update(task_id={'dict inserted': 1}, task=tasks.Task())
+
+    def test_bad_task(self):
+        """A non-Task task should raise an exception."""
+        with pytest.raises(TypeError):
+            tasks.update(task_id=1, task='not a task')
