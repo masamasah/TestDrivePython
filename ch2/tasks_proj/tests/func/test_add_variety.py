c1a9e5642227aa6b1b114ca67f5668926fcba223
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-18 18:07:43 +0900

    Page56 2-19
    (TestDrivePython) (anaconda2-4.1.0) hiratamasayuki-no-MacBook-Air:tasks_proj MasayukiHirata$ pytest -v tests/func/test_add_variety.py::test_add_6
    ============================================================ test session starts =============================================================
    platform darwin -- Python 3.7.2, pytest-4.2.0, py-1.7.0, pluggy-0.8.1 -- /Users/MasayukiHirata/anaconda3/envs/TestDrivePython/bin/python
    cachedir: .pytest_cache
    rootdir: /Users/MasayukiHirata/my-project/TestDrivePython/ch2/tasks_proj/tests, inifile: pytest.ini
    collected 3 items
    
    tests/func/test_add_variety.py::test_add_6[just summary] PASSED                                                                        [ 33%]
    tests/func/test_add_variety.py::test_add_6[summary/owner] PASSED                                                                       [ 66%]
    tests/func/test_add_variety.py::test_add_6[summary/owner/done] PASSED                                                                  [100%]
    
    ========================================================== 3 passed in 0.09 seconds ==========================================================

--- ch2/tasks_proj/tests/func/test_add_variety.py
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -71,6 +71,16 @@ class TestAdd():
         t_from_db = tasks.get(task_id)
         assert t_from_db.id == task_id
 
+@pytest.mark.parametrize('task', [pytest.param(Task('create'), id = 'just summary'),
+                                  pytest.param(Task('inspire', 'Michelle'), id = 'summary/owner'),
+                                  pytest.param(Task('encourage', 'Michelle', True), id = 'summary/owner/done')])
+def test_add_6(task):
+    """Demonstrate pytest.param and id."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
 def equivalent(t1, t2):
     """Check two tasks for equivalance."""
     return ((t1.summary == t2.summary) and (t1.owner == t2.owner) and (t1.done == t2.done))
