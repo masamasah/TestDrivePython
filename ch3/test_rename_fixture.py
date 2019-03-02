b27960c0d87d197cbcb96dd9b55183b7ceed416a
masayuki.hirata <masayuki.h0729@gmail.com>
2019-03-02 17:50:44 +0900

    Page77
    ------------------------------------------------------------ fixtures defined from test_rename_fixture ------------------------------------------------------------
    lue
        Return ultimate answer.

--- ch3/test_rename_fixture.py
+++ ch3/test_rename_fixture.py
@@ -2,7 +2,9 @@ import pytest
 
 @pytest.fixture(name='lue')
 def ultimate_anser_to_life_the_universe_and_everythong():
+    """Return ultimate answer."""
     return 42
 
 def test_everything(lue):
+    """Use the shorter name."""
     assert lue == 42
