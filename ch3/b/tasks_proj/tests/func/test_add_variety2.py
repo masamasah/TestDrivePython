60df3cbed860a06b4440f2f645ed4c330b9388fc
masayuki.hirata <masayuki.h0729@gmail.com>
2019-03-27 20:28:49 +0900

    Page78 3-14,15
    (TestDrivePython) (anaconda2-4.1.0) hiratamasayuki-no-MacBook-Air:func MasayukiHirata$ pytest -v test_add_variety2.py::test_add_a
    ============================= test session starts ==============================
    platform darwin -- Python 3.7.2, pytest-4.2.0, py-1.7.0, pluggy-0.8.1 -- /Users/MasayukiHirata/anaconda3/envs/TestDrivePython/bin/python
    cachedir: .pytest_cache
    rootdir: /Users/MasayukiHirata/my-project/TestDrivePython/ch3/b/tasks_proj/tests/func, inifile:
    collected 4 items
    
    test_add_variety2.py::test_add_a[a_task0] PASSED                         [ 25%]
    test_add_variety2.py::test_add_a[a_task1] PASSED                         [ 50%]
    test_add_variety2.py::test_add_a[a_task2] PASSED                         [ 75%]
    test_add_variety2.py::test_add_a[a_task3] PASSED                         [100%]
    
    =========================== 4 passed in 0.07 seconds ===========================

---
+++ ch3/b/tasks_proj/tests/func/test_add_variety2.py
@@ -0,0 +1,25 @@
+import pytest
+import tasks
+from tasks import Task
+
+tasks_to_try = (Task('sleep', done=True),
+                Task('wake', 'brian'),
+                Task('breathe', 'BRIAN', True),
+                Task('exercise', 'BrIaN', False))
+
+task_ids = ['TASK({}{}{})'.format(t.summary, t.owner, t.done) for t in tasks_to_try]
+
+def equivalent(t1, t2):
+    """Check two tasks for equivalance"""
+    return ((t1.summary == t2.summary)and(t1.owner == t2.owner)and(t1.done==t2.done))
+
+@pytest.fixture(params=tasks_to_try)
+def a_task(request):
+    """Using no ids."""
+    return request.param
+
+def test_add_a(tasks_db, a_task):
+    """Using a_task fixture (no ids)."""
+    task_id = tasks.add(a_task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, a_task)
